9a08f16e0080541d3a91825e15aa1e0803626d01
Jakob Fehle <jakob@fehle.net>
2019-12-16 14:55:05 +0100

    Adding base game

---
+++ study/game/Assets/Scripts/BeeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BeeController : MonoBehaviour
+{
+    public Transform target;
+
+    private GameController _gameController;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _beeAnimator;
+    private AudioSource _audioSource;
+    private float _distanceToTarget = Mathf.Infinity;
+    private Boolean _isSuccessful;
+
+    private static readonly int Attack = Animator.StringToHash("attack");
+    private static readonly int Move = Animator.StringToHash("move");
+    private static readonly int Idle = Animator.StringToHash("idle");
+    private static readonly int Die = Animator.StringToHash("die");
+
+    void Start()
+    {
+        _gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        _beeAnimator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _audioSource = GetComponent<AudioSource>();
+        _beeAnimator.SetBool(Idle, true);
+    }
+    
+    void Update()
+    {
+        if (_isSuccessful)
+        {
+            GetAway();
+        }
+        else
+        {
+            _distanceToTarget = Vector3.Distance(target.position, transform.position);
+            if (_distanceToTarget >= _navMeshAgent.stoppingDistance)
+            {
+                ChaseTarget();
+            } 
+            else if (_distanceToTarget <= _navMeshAgent.stoppingDistance)
+            {
+                AttackTarget();
+            }
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
+        _beeAnimator.SetBool(Idle, false);
+        _beeAnimator.SetBool(Move, true);
+        _navMeshAgent.SetDestination(target.position);
+    }
+
+    private void  AttackTarget()
+    {
+        _beeAnimator.SetBool(Move, false);
+        _beeAnimator.SetTrigger(Attack);
+        _beeAnimator.SetBool(Idle, true);
+    }
+
+    public void CollisionFromChild(Collider other)
+    {
+        if (_isSuccessful) return;
+        _isSuccessful = true;
+        _gameController.BeeScores();
+        _navMeshAgent.enabled = false;
+    }
+
+    private void GetAway()
+    {
+        _audioSource.Stop();
+        transform.position += Vector3.up * Time.deltaTime;
+        Destroy(gameObject, 8.0f);
+    }
+}
---
+++ study/game/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject beePrefab;
+    public int playerScore = 0;
+    public int beeScore = 0;
+    
+    private readonly Vector3[] _spawnPoints = new [] { new Vector3(-10, 0, 0), new Vector3(-10, 0, -40), 
+                                                       new Vector3(10, 0, -20), new Vector3(-30, 0, -20)};
+
+    private int _currentSpawnIndex = 0;
+    
+    void Start()
+    {
+        GameObject bee = Instantiate(beePrefab, _spawnPoints[_currentSpawnIndex++], Quaternion.identity);
+        bee.GetComponent<BeeController>().target = player.transform;
+    }
+
+    public void BeeScores()
+    {
+        beeScore += 1;
+        GameObject bee = Instantiate(beePrefab, _spawnPoints[_currentSpawnIndex++ % _spawnPoints.Length], Quaternion.identity);
+        bee.GetComponent<BeeController>().target = player.transform;
+    }
+
+    public void PlayerScores()
+    {
+        playerScore += 1;
+        GameObject bee = Instantiate(beePrefab, _spawnPoints[_currentSpawnIndex++ % _spawnPoints.Length], Quaternion.identity);
+        bee.GetComponent<BeeController>().target = player.transform;
+    }
+}
